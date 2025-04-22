c0fee094440c9a015b23a6246f88ca23dafe0564
Shitao Li <lst0451@gmail.com>
2025-04-21 22:36:30 -0400

    修改 Kafka K8S 部署, notification-service 监听端口

--- notification-service/src/main/java/io/lst/demo/notificationservice/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/io/lst/demo/notificationservice/config/KafkaConsumerConfig.java
@@ -1,10 +1,9 @@
 package io.lst.demo.notificationservice.config;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.lst.demo.common.event.UserRegisteredEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -20,10 +19,13 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, UserRegisteredEvent> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
@@ -34,7 +36,6 @@ public class KafkaConsumerConfig {
 
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
-
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, UserRegisteredEvent> kafkaListenerContainerFactory() {
